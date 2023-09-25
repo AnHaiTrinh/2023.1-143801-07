a3e3023994bd94e1b9831e622cd7c1d4b61e944e
NguyenKimHung2002 <107683285+NguyenKimHung2002@users.noreply.github.com>
2023-09-25 20:11:19 +0700

    Done

---
+++ src/Homework01/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+        view.setController(controller);
+        controller.start();
+        controller.actionListener();
+    }
+
+}
