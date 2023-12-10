52737e62da8c9ea1c05cc07fe968f0788772e870
NguyenKimHung2002 <107683285+NguyenKimHung2002@users.noreply.github.com>
2023-12-10 16:35:42 +0700

    temp

--- src/BTL/src/main/java/com/nhom7/login/Login.java
+++ src/BTL/src/main/java/com/nhom7/login/Login.java
@@ -1,4 +1,23 @@
 package com.nhom7.login;
 
-public class Login {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+    public static void main(String[] args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader((Login.class.getResource("login-view.fxml")));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 200);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
