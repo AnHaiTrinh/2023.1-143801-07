a7f0c2555fd4655829e9ba3ba01050a44f19f9e8
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-18 14:46:01 +0700

    add maven file poi, module

--- src/BTL/src/main/java/com/nhom7/import_data/SelectFileImportData.java
+++ src/BTL/src/main/java/com/nhom7/import_data/SelectFileImportData.java
@@ -1,11 +1,25 @@
 package com.nhom7.import_data;
 
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
 public class SelectFileImportData {
-    public  void  onMousePressedButtonAcceptImport(){
+    public  void  onMousePressedButtonAcceptImport(MouseEvent event) throws IOException {
 
     }
-    public void onMousePressedButtonSelectFileImport(){
-
+    public void onMousePressedButtonSelectFileImport(MouseEvent event) throws IOException{
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Chọn file import");
+        FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("data file", "*.csv", "*.xlsx");
+        fileChooser.getExtensionFilters().add(extensionFilter);
+        File file = fileChooser.showOpenDialog(stage);
+        ReadFileExcel readFileExcel = new ReadFileExcel();
+        readFileExcel.ReadFileData(file);
     }
-
 }
