4cf9f6662a6bccf8a10eb68620667b6bf40ce7e7
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-18 14:46:01 +0700

    add maven file poi, module

--- src/BTL/src/main/java/com/nhom7/import_data/ReadFileExcel.java
+++ src/BTL/src/main/java/com/nhom7/import_data/ReadFileExcel.java
@@ -1,53 +1,34 @@
 package com.nhom7.import_data;
 
-import com.nhom7.entity.AttendanceLog;
-import org.apache.poi.ss.usermodel.*;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-public class ReadFileExcel implements ReadFileService{
-    CheckFileExcel checkFileExcel = new CheckFileExcel();
-    @Override
-    public List<AttendanceLog> readDataFromFile(String url) throws IOException {
-        List<AttendanceLog> attendanceLogList = new ArrayList<>();
-        String result = checkFileExcel.checkErrorFile(url);
-        if( result.equals("success")){
-            try (InputStream inputStream = new URL(url).openStream()) {
-                Workbook workbook = WorkbookFactory.create(inputStream);
-                Sheet sheet = workbook.getSheetAt(0);
-                Iterator<Row> rowIterator = sheet.iterator();
-                boolean isFirstRow = true;
-                while (rowIterator.hasNext()) {
-                    Row row = rowIterator.next();
-                    if (isFirstRow) {
-                        isFirstRow = false;
-                        continue;
-                    }
-                    String maNV = row.getCell(0).getStringCellValue();
-                    LocalDate localDate = LocalDate.parse(row.getCell(1).getStringCellValue());
-                    LocalTime localTime = LocalTime.parse(row.getCell(2).getStringCellValue());
-                    String type = row.getCell(3).getStringCellValue();
-                    String machineID = row.getCell(4).getStringCellValue();
-                    AttendanceLog attendanceLog = new AttendanceLog(maNV, localDate, localTime, type, machineID);
-                    attendanceLogList.add(attendanceLog);
-                }
-                workbook.close();
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+public class ReadFileExcel {
+    public ReadFileExcel() {
+
+    }
+    public void ReadFileData(File fileExcel) throws IOException {
+        FileInputStream file = new FileInputStream(fileExcel);
+        XSSFWorkbook _Workbook = new XSSFWorkbook (file);
+        XSSFSheet _Sheet = _Workbook.getSheetAt (0);
+        Iterator<Row> _RowIterator = _Sheet.iterator();
+
+        while (((Iterator<?>) _RowIterator).hasNext()) {
+            Row _Row = _RowIterator.next();
+            Iterator<Cell> _CellIterator = _Row.cellIterator();
+
+            while (_CellIterator.hasNext()) {
+                Cell _Cell = _CellIterator.next();
+                System.out.print(_Cell.toString() + "\t");
             }
-            catch (Exception e){
-                //e.printStackTrace();
-            }
-            return attendanceLogList;
-        }else {
-            ConsoleMessageDisplay consoleMessageDisplay = new ConsoleMessageDisplay();
-            consoleMessageDisplay.displayMessage(result);
-            return  null;
+            System.out.println();
         }
     }
 }
