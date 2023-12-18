300c15fd76fb8c9e4876b3cbc6f389dd2f0c7737
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-18 16:32:06 +0700

    confilic from NTL

---
+++ src/BTL/src/main/java/com/nhom7/import_data/SelectFileImportData.java
@@ -0,0 +1,11 @@
+package com.nhom7.import_data;
+
+public class SelectFileImportData {
+    public  void  onMousePressedButtonAcceptImport(){
+
+    }
+    public void onMousePressedButtonSelectFileImport(){
+
+    }
+
+}
