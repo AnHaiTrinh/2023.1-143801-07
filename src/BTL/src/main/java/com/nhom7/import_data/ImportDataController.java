16397721b823806467954467e0a93a20d248c40a
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-18 16:23:21 +0700

    check rebase

--- src/BTL/src/main/java/com/nhom7/import_data/ImportDataController.java
+++ src/BTL/src/main/java/com/nhom7/import_data/ImportDataController.java
@@ -1,20 +1,4 @@
-package com.nhom7.import_data;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+package com.nhom7.importData;
 
 public class ImportDataController {
-    public void onMousePressedButtonImport(MouseEvent event) throws IOException {
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader(ImportDataController.class.getResource("SelectFileImportData.fxml"));
-        fxmlLoader.setController(new SelectFileImportData(new FileChooser(), new CheckFileExcel()));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.show();
-    }
 }
