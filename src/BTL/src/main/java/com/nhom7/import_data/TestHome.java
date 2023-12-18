73fe2febe8e37bbd23ad1608e7f694862cf87050
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-18 16:23:53 +0700

    check rebase

--- src/BTL/src/main/java/com/nhom7/import_data/TestHome.java
+++ src/BTL/src/main/java/com/nhom7/import_data/TestHome.java
@@ -1,10 +1,10 @@
 package com.nhom7.import_data;
 
-import com.nhom7.check_value.IsValidValue;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,7 +12,6 @@ public class TestHome extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TestHome.class.getResource("HomeImportData.fxml"));
-        fxmlLoader.setController(new ImportDataController());
         Scene scene = new Scene(fxmlLoader.load(), 864, 559);
         stage.setTitle("Hello!");
         stage.setScene(scene);
