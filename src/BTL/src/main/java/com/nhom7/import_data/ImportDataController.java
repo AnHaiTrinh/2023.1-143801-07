d2f12879a07bcc9f9efa9a6683b423e4184517fc
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-18 16:32:06 +0700

    confilic from NTL

--- src/BTL/src/main/java/com/nhom7/import_data/ImportDataController.java
+++ src/BTL/src/main/java/com/nhom7/import_data/ImportDataController.java
@@ -1,4 +1,19 @@
 package com.nhom7.import_data;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ImportDataController {
+    public void onMousePressedButtonImport(MouseEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(ImportDataController.class.getResource("SelectFileImportData.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
 }
