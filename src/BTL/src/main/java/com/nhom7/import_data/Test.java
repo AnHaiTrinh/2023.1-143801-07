a5decc61df3f3672a8137cba8f1a0de092bd14d1
kaisacht <leanhvut1k27cht@gmail.com>
2023-12-20 16:25:34 +0700

    fix class test import

--- src/BTL/src/main/java/com/nhom7/import_data/Test.java
+++ src/BTL/src/main/java/com/nhom7/import_data/Test.java
@@ -1,6 +1,5 @@
 package com.nhom7.import_data;
 
-import com.nhom7.check_value.IsValidValue;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,16 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class TestHome extends Application {
+public class Test extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(TestHome.class.getResource("HomeImportData.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Test.class.getResource("HomeImportData.fxml"));
         fxmlLoader.setController(new ImportDataController());
         Scene scene = new Scene(fxmlLoader.load(), 864, 559);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
